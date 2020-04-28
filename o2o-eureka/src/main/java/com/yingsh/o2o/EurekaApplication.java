e3b6c8f551602c160675172d35dea0646b0847c8
qintao <472105484@qq.com>
2020-04-28 18:33:47 +0800

    first commit

---
+++ o2o-eureka/src/main/java/com/yingsh/o2o/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.yingsh.o2o;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * <h1>Eureka 启动入口</h1>
+ * Created by Qinyi.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+
+}
---
+++ o2o-gateway/src/main/java/com/yingsh/o2o/ZuulGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.yingsh.o2o;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by qt on 2020/4/27.
+ */
+@EnableZuulProxy
+@SpringCloudApplication
+public class ZuulGatewayApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ZuulGatewayApplication.class,args);
+    }
+
+}
